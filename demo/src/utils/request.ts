ddc076f19fcac451cc3ffdc935fe51d75bea6e67
xilanhuaer <1445106428@qq.com>
2023-10-23 09:29:28 +0000

    增加默认请求头

--- demo/src/utils/request.ts
+++ demo/src/utils/request.ts
@@ -3,6 +3,9 @@ import axios from 'axios'
 const request = axios.create({
     baseURL: 'http://111.230.89.67:82',
     timeout: 5000,
+    headers: {
+        "Content-Type": "application/json"
+    }
 })
 
 export default request
